59800cea4cbde7d65556b9ad15e876201e1abd8f
camcast04 <cam.cast04@gmail.com>
2024-04-10 19:50:32 -0400

    added create

--- server.js
+++ server.js
@@ -1,21 +1,37 @@
 var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
+const express = require('express');
+const path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const mongoose = require('mongoose');
+const Climbspot = require('./models/climbspot');
+const climbspot = require('./models/climbspot');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
-var app = express();
+const methodOverride = require('method-override');
+
+mongoose.connect('mongodb://localhost:27017/climb-on', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log('Database connected');
+});
+
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
+app.use(methodOverride('_method'));
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -23,12 +39,34 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
+//climbspot index
+app.get('/climbspots', async (req, res) => {
+  // find all the climbspots (grab them)
+  const climbspots = await climbspots.find({});
+  // passing it through to template and render them
+  res.render('climbspots/index', { climbspots });
+});
+
+// form to create new climbspots
+app.get('/climbspots/new', (req, res) => {
+  res.render('climbspots/new'); // rendering a form
+});
+
+//payload from form (req.body)
+app.post('/climbspots', async (req, res) => {
+  //creating a new climbspot
+  const climbspot = new Climbspot(req.body.climbspot);
+  //saving
+  await climbspot.save();
+  res.redirect(`/climbspots/${climbspot._id}`);
+});
+
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
