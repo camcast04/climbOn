a34202aa1921cd6d42850e0d12fc84b8f3bd6a61
camcast04 <cam.cast04@gmail.com>
2024-04-11 13:00:35 -0400

    edit ejs views

--- server.js
+++ server.js
@@ -103,6 +103,24 @@ app.delete('/climbspots/:id', async (req, res) => {
   res.redirect('/climbspots');
 });
 
+app.post('/climbspots/:id/reviews', async (req, res) => {
+  const climbspot = await Climbspot.findById(req.params.id);
+  const review = new Review(req.body.review);
+  climbspot.reviews.push(review);
+  await review.save();
+  await climbspot.save();
+  res.redirect(`/climbspots/${climbspot._id}`);
+});
+
+//delete reviews
+//$pull operator removes from an existing array all instances of a value or values that match a specified condition
+app.delete('/climbspots/:id/reviews/:reviewId', async (req, res) => {
+  const { id, reviewId } = req.params;
+  await Climbspot.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
+  await Review.findByIdAndDelete(reviewId);
+  res.redirect(`/climbspots/${id}`);
+});
+
 //*********************
 //*********************
 // error handler
